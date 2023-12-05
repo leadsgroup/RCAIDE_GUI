5f8cbfbcb7897702b3449841679a736d890fd3a0
chris-z4 <146781842+chris-z4@users.noreply.github.com>
2023-12-04 19:08:35 -0600

    File Rearranging
    Moved frames under geometry folder

--- tabs/geometry/geometry.py
+++ tabs/geometry/geometry.py
@@ -2,12 +2,12 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QComboBox
 
 from widgets.color import Color
-from tabs.frames.default_frame import DefaultFrame
-from tabs.frames.wings_frame import WingsFrame
-from tabs.frames.fuselage_frame import FuselageFrame
-from tabs.frames.nacelle_frame import NacellesFrame
-from tabs.frames.landing_gear_frame import LandingGearFrame
-from tabs.frames.energy_network_frame import EnergyNetworkFrame
+from tabs.geometry.frames.default_frame import DefaultFrame
+from tabs.geometry.frames.wings_frame import WingsFrame
+from tabs.geometry.frames.fuselage_frame import FuselageFrame
+from tabs.geometry.frames.nacelle_frame import NacellesFrame
+from tabs.geometry.frames.landing_gear_frame import LandingGearFrame
+from tabs.geometry.frames.energy_network_frame import EnergyNetworkFrame
 
 
 class GeometryWidget(QWidget):
