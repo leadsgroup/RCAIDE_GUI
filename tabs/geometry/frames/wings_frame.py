b4e43f3c24a83738d4a9c3e90fe0d9a4ff3d8c90
JaredDost <145399906+JaredDost@users.noreply.github.com>
2024-02-05 19:20:38 -0600

    Removed Wing Title, Added Wing Name

--- tabs/geometry/frames/wings_frame.py
+++ tabs/geometry/frames/wings_frame.py
@@ -3,10 +3,10 @@
 # ------------------------------------------------------------------------------------------------------------------------------------
 
 # RCAIDE imports
-# '''Will use later'''
+'''Will use later'''
 
 # QT imports
-from PyQt6.QtCore import QTimer
+from PyQt6.QtCore import Qt, QTimer
 from PyQt6.QtGui import QDoubleValidator, QFont
 from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QGridLayout, QLineEdit, QMessageBox, \
     QScrollArea
@@ -30,24 +30,29 @@ class WingsFrame(QWidget):
 
         # Header layout with bold label and buttons
         header_layout = QHBoxLayout()
-        wing_title_label = QLabel("Add Wing Component")
+        wing_name_label = QLabel("Wing Name:")
+        wing_name_entry = QLineEdit(self)
+        wing_name_entry.setFixedWidth(265)
         bold_font = QFont()
         bold_font.setBold(True)
-        wing_title_label.setFont(bold_font)
-        header_layout.addWidget(wing_title_label)
+        wing_name_label.setFont(bold_font)        
+        header_layout.addWidget(wing_name_label)
+        header_layout.addWidget(wing_name_entry)
 
         append_button = QPushButton("Append Wing Data", self)
         delete_button = QPushButton("Delete Wing Data", self)
+        append_button.setFixedWidth(341)
+        delete_button.setFixedWidth(341)        
         append_button.clicked.connect(self.append_data)
         delete_button.clicked.connect(self.delete_data)
         header_layout.addWidget(append_button)
         header_layout.addWidget(delete_button)
         content_layout.addLayout(header_layout)
-        content_layout.addWidget(Color("lightblue"))
+        #content_layout.addWidget(Color("lightblue"))
 
         # Grid layout for wing components
         grid_layout = QGridLayout()
-        wing_data_labels = ["Wing Name", "Taper Ratio", "Dihedral", "Aspect Ratio", "Thickness to Chord",
+        wing_data_labels = ["Taper Ratio", "Dihedral", "Aspect Ratio", "Thickness to Chord",
                             "Aerodynamic Center",
                             "Exposed Root Chord Offset", "Total Length", "Spans Projected", "Spans Total",
                             "Areas Reference",
@@ -60,16 +65,11 @@ class WingsFrame(QWidget):
         for index, label in enumerate(wing_data_labels):
             row, col = divmod(index, 3)
             line_edit = QLineEdit(self)
-            line_edit.setValidator(QDoubleValidator())
             line_edit.setFixedWidth(100)
             grid_layout.addWidget(QLabel(label + ":"), row, col * 2)
             grid_layout.addWidget(line_edit, row, col * 2 + 1)
             self.data_values['wing_' + label] = line_edit
         content_layout.addLayout(grid_layout)
-
-        # -------------------------------------------------------------------------------------------------------------------------------
-        #   Preliminary Wing Shape Simulation
-        # -------------------------------------------------------------------------------------------------------------------------------        
 
         # -------------------------------------------------------------------------------------------------------------------------------
         #   Control Surfaces
@@ -81,13 +81,15 @@ class WingsFrame(QWidget):
         # Layout for control surface components with bold label
         lower_layout = QHBoxLayout()
         cs_title_label = QLabel("Add Control Surface Component")
-        cs_title_label.setFont(bold_font)  # Apply the bold font to the label
+        cs_title_label.setFont(bold_font)
         lower_layout.addWidget(cs_title_label)
 
         '''Add commands to these buttons later'''
-
+        
         append_button2 = QPushButton("Append Control Surface Data", self)
         delete_button2 = QPushButton("Delete Control Surface Data", self)
+        append_button2.setFixedWidth(341)
+        delete_button2.setFixedWidth(341)         
         lower_layout.addWidget(append_button2)
         lower_layout.addWidget(delete_button2)
         content_layout.addLayout(lower_layout)
@@ -103,7 +105,6 @@ class WingsFrame(QWidget):
         for index, label in enumerate(cs_data_labels):
             row, col = divmod(index, 3)
             line_edit = QLineEdit(self)
-            line_edit.setValidator(QDoubleValidator())
             line_edit.setFixedWidth(100)
             cs_grid_layout.addWidget(QLabel(label + ":"), row, col * 2)
             cs_grid_layout.addWidget(line_edit, row, col * 2 + 1)
@@ -120,7 +121,7 @@ class WingsFrame(QWidget):
         main_layout.addWidget(scroll_area)
         self.setLayout(main_layout)
 
-    '''Functions subject to change due to RCAIDE integration'''
+    '''Functions subject to change due to RCAIDE integration''' 
 
     def append_data(self):
         """ Append data action. """
@@ -152,42 +153,3 @@ class WingsFrame(QWidget):
         timer.setSingleShot(True)
         timer.timeout.connect(popup.close)
         timer.start(2000)
-
-        # -------------------------------------------------------------------------------------------------------------------------------
-        #   Wing Sections
-        # -------------------------------------------------------------------------------------------------------------------------------
-
-    def add_wing_section(self):
-        """Add a new fuselage section with input boxes for Percent X Location, Percent Z Location, Height, and Width."""
-        additional_section_layout = QGridLayout()
-
-        percent_x_location = QLineEdit(self)
-        percent_x_location.setValidator(QDoubleValidator())
-        percent_x_location.setFixedWidth(100)
-
-        percent_z_location = QLineEdit(self)
-        percent_z_location.setValidator(QDoubleValidator())
-        percent_z_location.setFixedWidth(100)
-
-        height = QLineEdit(self)
-        height.setValidator(QDoubleValidator())
-        height.setFixedWidth(100)
-
-        width = QLineEdit(self)
-        width.setValidator(QDoubleValidator())
-        width.setFixedWidth(100)
-
-        additional_section_layout.addWidget(QLabel("Percent X Location:"), 0, 0)
-        additional_section_layout.addWidget(percent_x_location, 0, 1)
-
-        additional_section_layout.addWidget(QLabel("Percent Z Location:"), 1, 0)
-        additional_section_layout.addWidget(percent_z_location, 1, 1)
-
-        additional_section_layout.addWidget(QLabel("Height:"), 0, 2)
-        additional_section_layout.addWidget(height, 0, 3)
-
-        additional_section_layout.addWidget(QLabel("Width:"), 1, 2)
-        additional_section_layout.addWidget(width, 1, 3)
-
-        # delete_section_button = QPushButton("Delete Fuselage Section", self)
-        # append_data_button = QPushButton("Append Fuselage Section Data", self)
