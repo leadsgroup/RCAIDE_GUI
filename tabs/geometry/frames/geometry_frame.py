01d88e6044a1346b876953210276881f44a2b15b
Aadit Kolar <aadit.kolar@gmail.com>
2024-02-14 17:56:19 -0600

    Created GeometryFrame class

---
+++ tabs/geometry/frames/geometry_frame.py
@@ -0,0 +1,9 @@
+class GeometryFrame:
+    def load_data(self, data, index):
+        pass
+
+    def set_save_function(self, function):
+        pass
+
+    def set_tab_index(self, index):
+        pass
