4df832b6c73f585085fecbc8d52b8717cb6d5966
chris-z4 <146781842+chris-z4@users.noreply.github.com>
2024-02-18 14:09:31 -0600

    Fuselage Visuals Fixed
    Simplified Buttons and Naming System.

--- tabs/geometry/frames/fuselage_frame.py
+++ tabs/geometry/frames/fuselage_frame.py
@@ -33,18 +33,18 @@ class FuselageFrame(QWidget):
 
         # Create a horizontal layout for the label and buttons
         header_layout = QHBoxLayout()
-        label = QLabel("<u><b>Fuselage Frame</b></u>")
+        label = QLabel("<u><b>Main Fuselage Frame</b></u>")
         header_layout.addWidget(label)
 
         # Add buttons for appending and deleting data
-        append_button = QPushButton("Append Data", self)
-        delete_button = QPushButton("Delete Data", self)
+        #append_button = QPushButton("Append Data", self)
+        delete_button = QPushButton("Delete Main Fuselage Frame Data", self)
 
-        append_button.clicked.connect(self.append_data)
+        #append_button.clicked.connect(self.append_data)
         delete_button.clicked.connect(self.delete_data)
 
-        header_layout.addWidget(append_button)
-        header_layout.addWidget(delete_button)
+        #header_layout.addWidget(append_button)
+        
 
         layout.addLayout(header_layout)
         layout.addWidget(Color("lightblue"))
@@ -52,6 +52,8 @@ class FuselageFrame(QWidget):
         # Create a grid layout with 3 columns for main
         grid_layout = QGridLayout()
 
+        grid_layout.addWidget(delete_button,8,3,1,2)
+        
         # List of data labels for the main fuselage section
         main_data_labels = ["Fineness Nose", "Fineness Tail", "Lengths Nose", "Lengths Tail", "Lengths Cabin",
                             "Lengths Total", "Lengths Forespace", "Lengths Aftspace", "Width", "Heights Maximum",
@@ -59,7 +61,7 @@ class FuselageFrame(QWidget):
                             "Areas Side Projected", "Area Wetted", "Area Front Projected", "Effective Diameter"]
 
         for index, label in enumerate(main_data_labels):
-            row, col = divmod(index, 3)
+            row, col = divmod(index, 2)
             line_edit = QLineEdit(self)
             line_edit.setValidator(QDoubleValidator())
 
@@ -107,8 +109,8 @@ class FuselageFrame(QWidget):
         button_layout.addWidget(add_section_button)
 
         # Append All Fuselage Section Data Button
-        append_all_data_button = QPushButton("Append All Fuselage Section Data", self)
-        #append_all_data_button.clicked.connect(self.append_all_data)
+        append_all_data_button = QPushButton("Save All Fuselage Data", self)
+        append_all_data_button.clicked.connect(self.append_all_data)
         button_layout.addWidget(append_all_data_button)
 
         # Add the button layout to the main layout
@@ -127,11 +129,20 @@ class FuselageFrame(QWidget):
         # Set the layout to the main window/widget
         self.setLayout(layout_scroll)
 
-    def append_data(self):
+    def append_all_data(self):
         """Append the entered data to a list or perform any other action."""
-        entered_data = self.get_data_values()
-        print("Appending Data:", entered_data)
+        main_data = self.get_data_values()  # Get data from the main fuselage section
+    
+        # Collect data from additional fuselage_widget
+        additional_data = []
+        for index in range(self.additional_data_values.count()):
+            widget = self.additional_data_values.itemAt(index).widget()
+            additional_data.append(widget.get_data_values())
+    
+        print("Main Fuselage Data:", main_data)
+        print("Additional Fuselage Data:", additional_data)
         show_popup("Data Saved!", self)
+
 
     def delete_data(self):
         """Delete the entered data or perform any other action."""
@@ -153,7 +164,7 @@ class FuselageFrame(QWidget):
         self.additional_data_values.addWidget(FuselageWidget(self.additional_data_values.count(), self.on_delete_button_pressed))
 
     def on_delete_button_pressed(self, index):
-        # TODO: Update indices of the nacelle widgets after the deleted index
+
         self.additional_data_values.itemAt(index).widget().deleteLater()
         self.additional_data_values.removeWidget(self.additional_data_values.itemAt(index).widget())
         self.additional_data_values.update()
@@ -162,130 +173,3 @@ class FuselageFrame(QWidget):
         for i in range(index, self.additional_data_values.count()):
             self.additional_data_values.itemAt(i).widget().index = i
             print("Updated Index:", i)
-    # ================================================================================================================================================
-
-    # Fuselage Section
-
-    # ================================================================================================================================================
-
-    #def add_fuselage_section(self):
-        #"""Add a new fuselage section with input boxes for Percent X Location, Percent Z Location, Height, and Width."""
-        #additional_section_layout = QGridLayout()
-
-        ## Add segment number label
-        #segment_label = QLabel()
-        #segment_label.setText(f"<b><u>Segment {len(self.additional_data_values)}</b></u>")
-        #additional_section_layout.addWidget(segment_label)
-
-        #percent_x_location = QLineEdit(self)
-        #percent_x_location.setValidator(QDoubleValidator())
-        #percent_x_location.setFixedWidth(100)
-
-        #percent_z_location = QLineEdit(self)
-        #percent_z_location.setValidator(QDoubleValidator())
-        #percent_z_location.setFixedWidth(100)
-
-        #height = QLineEdit(self)
-        #height.setValidator(QDoubleValidator())
-        #height.setFixedWidth(100)
-
-        #width = QLineEdit(self)
-        #width.setValidator(QDoubleValidator())
-        #width.setFixedWidth(100)
-
-        #additional_section_layout.addWidget(QLabel("Percent X Location:"), 1, 0)
-        #additional_section_layout.addWidget(percent_x_location, 1, 4, 1, 2)
-
-        #additional_section_layout.addWidget(QLabel("Percent Z Location:"), 2, 0)
-        #additional_section_layout.addWidget(percent_z_location, 2, 4, 1, 2)
-
-        #additional_section_layout.addWidget(QLabel("Height:"), 1, 2 * 3)
-        #additional_section_layout.addWidget(height, 1, 9, 1, 4)
-
-        #additional_section_layout.addWidget(QLabel("Width:"), 2, 2 * 3)
-        #additional_section_layout.addWidget(width, 2, 9, 1, 4)
-
-        #delete_section_button = QPushButton("Delete Fuselage Section", self)
-        #append_data_button = QPushButton("Append Fuselage Section Data", self)
-
-        #delete_section_button.clicked.connect(self.delete_and_display_data)
-
-        ## Connect the "Append Fuselage Section Data" button to a lambda function
-        ## that captures the index of the button and calls append_section_data with that index
-        #append_data_button.clicked.connect(
-            #lambda _, index=len(self.additional_data_values): self.append_section_data(index))
-
-        ## Add buttons to the layout
-        #additional_section_layout.addWidget(delete_section_button, 1, 4 * 3 + 1, 1, 2)
-        #additional_section_layout.addWidget(append_data_button, 2, 4 * 3 + 1, 1, 2)
-
-        #self.additional_layout.addLayout(additional_section_layout)
-        #self.additional_data_values.append({"Percent X Location": percent_x_location,
-                                            #"Percent Z Location": percent_z_location,
-                                            #"Height": height,
-                                            #"Width": width})
-
-        #print("Number of sections after addition:", len(self.additional_data_values))  # Debugging statement
-
-    #def append_section_data(self, section_index):
-        #"""Append the entered data for the specified fuselage section."""
-        #entered_data = self.get_section_data_values(section_index)
-        #print("Appending Section Data for section", section_index, ":", entered_data)
-
-        #show_popup("Section Data Saved!", self)
-
-    #def get_section_data_values(self, section_index):
-        #"""Retrieve the entered data values for the specified fuselage section."""
-        #data_values = self.additional_data_values[section_index]
-        #return {label: float(line_edit.text()) if line_edit.text() else 0.0
-                #for label, line_edit in data_values.items()}
-
-    #def delete_section(self):
-        #"""Deletes the fuselage section."""
-        #try:
-            #print("Number of sections before deletion:", len(self.additional_data_values))  # Debugging statement
-            #if self.additional_data_values:
-                ## Remove widgets from layout
-                #layout_item = self.additional_layout.itemAt(self.additional_layout.count() - 1)
-                #if layout_item is not None:
-                    #for i in reversed(range(layout_item.layout().count())):
-                        #widget = layout_item.layout().itemAt(i).widget()
-                        #layout_item.layout().removeWidget(widget)
-                        #widget.deleteLater()
-
-                    ## Remove the layout item if the layout is empty
-                    #if layout_item.layout().count() == 0:
-                        #self.additional_layout.removeItem(layout_item)
-
-                ## Remove the data from the list
-                #self.additional_data_values.pop()
-            #else:
-                #print("No sections to delete.")
-
-            ## Debugging statement to check the number of sections after deletion
-            #print("Number of sections after deletion:", len(self.additional_data_values))
-        #except Exception as e:
-            #print(f"An error occurred while deleting section: {e}")
-
-    #def display_data(self):
-        #"""Displays the data after a section is deleted."""
-        ## \\print("Updated Section Data after deletion:", self.get_section_data_values())
-        #pass
-
-    #def delete_and_display_data(self):
-        #"""Combining display_data and delete_section function so button can call both."""
-        #self.delete_section()
-        #self.display_data()
-
-    #def append_all_data(self):
-        #"""Append the entered data for the additional fuselage section."""
-        #all_entered_data = self.get_all_data_values()
-        #print("Appending All Data:", all_entered_data)
-
-        #show_popup("Section Data Saved!", self)
-
-    #def get_all_data_values(self):
-        #"""Retrieve the entered data values for additional fuselage sections."""
-        #return [{label + f"_{i}": float(line_edit.text()) if line_edit.text() else 0.0
-                 #for label, line_edit in data_values.items()}
-                #for i, data_values in enumerate(self.additional_data_values, start=1)]
--- tabs/geometry/widgets/fuselage_widget.py
+++ tabs/geometry/widgets/fuselage_widget.py
@@ -1,81 +1,69 @@
 import os
 
 from PyQt6.QtGui import QDoubleValidator
-from PyQt6.QtWidgets import QWidget, QPushButton, QLabel, QLineEdit, QGridLayout
+from PyQt6.QtWidgets import QWidget, QFileDialog, QPushButton, QLabel, QLineEdit, QGridLayout
+
 
 class FuselageWidget(QWidget):
     def __init__(self, index, on_delete):
         super(FuselageWidget, self).__init__()
 
-        self.additional_data_values = {}
+        self.data_values = {}
         self.index = index
         self.on_delete = on_delete
 
-        self.grid_layout = QGridLayout()
-        self.setLayout(self.grid_layout)
+        grid_layout = QGridLayout()
+ 
+ 
+        self.section_name_edit = QLineEdit(self)
+        grid_layout.addWidget(QLabel("<u><b>Fuselage Section: </b></u>"), 0, 0)
+        grid_layout.addWidget(self.section_name_edit, 0, 1, 1, 2)
 
         # List of data labels
         data_labels = [
             "Percent X Location",
             "Percent Z Location",
             "Height",
-            "Width"
+            "Width",
         ]
 
         # Create QLineEdit frames with QDoubleValidator for numerical input
-        for idx, label in enumerate(data_labels):
-            row, col = divmod(idx, 2)
+        for index, label in enumerate(data_labels):
+            row, col = divmod(index , 2)
             line_edit = QLineEdit(self)
             line_edit.setValidator(QDoubleValidator())
 
             # Set the width of the line edit
             line_edit.setFixedWidth(100)  # Adjust the width as needed
 
-            self.grid_layout.addWidget(QLabel(label + ":"), row + 1, col * 3)
-            self.grid_layout.addWidget(line_edit, row + 1, col * 3 + 1, 1, 2)
+            grid_layout.addWidget(QLabel(label + ":"), row+1, col * 3)
+            grid_layout.addWidget(line_edit, row+1, col * 3 + 1, 1, 2)
 
             # Store a reference to the QLineEdit in the dictionary
-            self.additional_data_values[label] = line_edit
+            self.data_values[label] = line_edit
 
-        # Add an append button
-        row, col = (1, 2)
-        self.append_button = QPushButton("Append Fuselage", self)
-        self.append_button.clicked.connect(self.append_button_pressed)
-        self.grid_layout.addWidget(self.append_button, row + 1, col * 3, 1, 2)
+
 
         # Add a delete button
-        row, col = divmod(len(data_labels) + 1, 3)
-        self.delete_button = QPushButton("Delete Fuselage", self)
-        self.delete_button.clicked.connect(self.delete_button_pressed)
-        self.grid_layout.addWidget(self.delete_button, row, col * 3, 1, 2)
+        row, col = divmod(len(data_labels) + 2, 1)
+        delete_button = QPushButton("Delete Fuselage Segment", self)
+        delete_button.clicked.connect(self.delete_button_pressed)
+        grid_layout.addWidget(delete_button, row, col * 3, 1, 5)
+
+        self.setLayout(grid_layout)
 
 
-
-
-    def append_button_pressed(self):
-        """Append the entered data for the specified fuselage section."""
-        # Retrieve the entered data from the QLineEdit widgets
-        percent_x_location = float(self.additional_data_values["Percent X Location"].text())
-        percent_z_location = float(self.additional_data_values["Percent Z Location"].text())
-        height = float(self.additional_data_values["Height"].text())
-        width = float(self.additional_data_values["Width"].text())
-    
-        print(f"Append Data for Fuselage Segment {self.index}: Percent X Location: {percent_x_location}, Percent Z Location: {percent_z_location}, Height: {height}, Width: {width}")
-    
-        # Increase the number of segments
-        self.num_segments += 1
-        # Update subtitles
-        self.update_subtitles()
-    
     def delete_button_pressed(self):
-        print(f"Delete button pressed for Fuselage Segment {self.index}")
-    
+        print("Delete button pressed")
+
         if self.on_delete is None:
             print("on_delete is None")
             return
-    
+
         self.on_delete(self.index)
-        # Decrease the number of segments
-        self.num_segments -= 1
-        # Update subtitles after deletion
-        self.update_subtitles()
+        
+    def get_data_values(self):
+        title = self.section_name_edit.text()
+        data_values = {label: float(line_edit.text()) if line_edit.text() else 0.0
+                       for label, line_edit in self.data_values.items()}
+        return title, data_values
