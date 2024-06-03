24c4aebbddf4ebdf5127e830aad7c0df606da67d
Aadit Kolar <aadit.kolar@gmail.com>
2024-06-03 14:27:14 -0500

    Create EnergyNetworkWidget to standardize

---
+++ tabs/geometry/frames/energy_network/energy_network_widget.py
@@ -0,0 +1,15 @@
+from PyQt6.QtWidgets import QWidget
+
+
+class EnergyNetworkWidget(QWidget):
+    def __init__(self):
+        pass
+
+    def create_rcaide_structure(self, data):
+        pass
+
+    def load_data_values(self, data):
+        pass
+
+    def get_data_values(self):
+        pass
