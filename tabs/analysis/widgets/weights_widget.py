6a1f68ed3a84704ccd1ab3366a77e47a32d72087
Aadit Kolar <aadit.kolar@gmail.com>
2024-06-19 00:44:00 -0500

    Completed WeightsWidget

--- tabs/analysis/widgets/weights_widget.py
+++ tabs/analysis/widgets/weights_widget.py
@@ -1,11 +1,22 @@
-from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QScrollArea, \
-    QFrame, QSpacerItem, QSizePolicy
+from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout
 
-from utilities import create_line_bar
+from utilities import Units, create_line_bar
+from widgets import DataEntryWidget
 
-# TODO: Add weights analysis
 
 class WeightsWidget(QWidget):
+    data_units_labels = [
+        ("Max Takeoff Weight", Units.Mass),
+        ("Actual Takeoff Weight", Units.Mass),
+        ("Operating Empty Weight", Units.Mass),
+        ("Maximum Zero Fuel Weight", Units.Mass),
+        ("Cargo Weight", Units.Mass),
+        ("Ultimate Load", Units.Unitless),
+        ("Limit Load", Units.Unitless),
+        ("Reference Area", Units.Area),
+        ("Passengers", Units.Count),
+    ]
+
     def __init__(self):
         super(WeightsWidget, self).__init__()
         self.data_values = {}
@@ -13,5 +24,9 @@ class WeightsWidget(QWidget):
 
         self.main_layout.addWidget(QLabel("<b>Weights</b>"))
         self.main_layout.addWidget(create_line_bar())
+
+        self.data_entry_widget = DataEntryWidget(self.data_units_labels)
+        self.main_layout.addWidget(self.data_entry_widget)
+
         self.main_layout.addWidget(create_line_bar())
         self.setLayout(self.main_layout)
