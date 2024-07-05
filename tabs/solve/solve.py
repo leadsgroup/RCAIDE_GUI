26fb433f0f727f7d6e8505eb06289c1120aa54a5
Aadit Kolar <aadit.kolar@gmail.com>
2024-07-05 15:21:50 -0500

    Created solve tab plot selector

--- tabs/solve/solve.py
+++ tabs/solve/solve.py
@@ -1,27 +1,95 @@
-from PyQt6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QPushButton
+from PyQt6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QTreeWidget, QPushButton, QTreeWidgetItem, QHeaderView
+from PyQt6.QtCore import Qt
 
-from widgets.color import Color
+
+class SolveWidget(QWidget):
+    def __init__(self):
+        super(SolveWidget, self).__init__()
+
+        base_layout = QHBoxLayout()
+        tree_layout = QVBoxLayout()
+        main_layout = QVBoxLayout()
+
+        solve_button = QPushButton("Solve")
+        solve_button.clicked.connect(self.run_solve)
+
+        self.tree = QTreeWidget()
+        self.init_tree()
+        tree_layout.addWidget(self.tree)
+
+        base_layout.addLayout(tree_layout, 3)
+        base_layout.addLayout(main_layout, 7)
+
+        base_widget = QWidget()
+        base_widget.setLayout(base_layout)
+
+        self.setLayout(base_layout)
+
+    def init_tree(self):
+        self.tree.setColumnCount(2)
+        self.tree.setHeaderLabels(["Plot Options", "Enabled"])
+        self.tree.header().setSectionResizeMode(
+            0, QHeaderView.ResizeMode.ResizeToContents)
+        for category, options in self.plot_options.items():
+            category_item = QTreeWidgetItem([category])
+            self.tree.addTopLevelItem(category_item)
+            for option in options:
+                option_item = QTreeWidgetItem([option])
+                option_item.setCheckState(1, Qt.CheckState.Checked)
+                category_item.addChild(option_item)
+
+    def run_solve(self):
+        pass
+
+    plot_options = {
+        "Geometry": [
+            "Plot 3D Vehicle",
+            "Plot 3D Energy Network",
+            "Generate 3D Vehicle Geometry Data",
+            "Plot 3D Rotor",
+            "Generate 3D Blade Points",
+            "Plot 3D Nacelle",
+            "Generate 3D Basic Nacelle Points",
+            "Generate 3D BOR Nacelle Points",
+            "Generate 3D Stack Nacelle Points",
+            "Plot 3D Wing",
+            "Generate 3D Wing Points",
+            "Plot 3D Vehicle VLM Panelization",
+            "Plot Airfoil",
+            "Plot Rotor",
+        ],
+        "Aerodynamics": [
+            "Plot Airfoil Boundary Layer Properties",
+            "Plot Airfoil Polar Files",
+            "Plot Airfoil Polars",
+            "Plot Airfoil Surface Forces",
+            "Plot Aerodynamic Coefficients",
+            "Plot Aerodynamic Forces",
+            "Plot Drag Components",
+            "Plot Lift Distribution",
+            "Plot Rotor Disc Inflow",
+            "Plot Rotor Disc Performance",
+            "Plot Rotor Performance",
+            "Plot Disc and Power Loading",
+            "Plot Rotor Conditions",
+        ],
+        "Energy": [
+            "Plot Fuel Consumption",
+            "Plot Altitude SFC Weight",
+            "Plot Propulsor Throttles",
+        ],
+        "Mission": [
+            "Plot Aircraft Velocities",
+            "Plot Flight Conditions",
+            "Plot Flight Trajectory",
+        ],
+        "Stability": [
+            "Plot Flight Forces and Moments",
+            "Plot Longitudinal Stability",
+            "Plot Lateral Stability",
+        ],
+    }
 
 
 def get_widget() -> QWidget:
-    base_layout = QHBoxLayout()
-    main_layout = QVBoxLayout()
-
-    tree_frame = Color("pink")
-    main_frame = Color("pink")
-    main_extra_frame = Color("pink")
-
-    solve_button = QPushButton("SOLVE")
-
-    main_layout.addWidget(main_frame, 7)
-    main_layout.addWidget(solve_button, 2)
-    main_layout.addWidget(main_extra_frame, 3)
-    base_layout.addWidget(tree_frame, 1)
-    base_layout.addLayout(main_layout, 4)
-
-    main_layout.setSpacing(3)
-    base_layout.setSpacing(3)
-
-    base_widget = QWidget()
-    base_widget.setLayout(base_layout)
-    return base_widget
+    return SolveWidget()
