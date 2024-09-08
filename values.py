2129917dfa68e755268cab2025ae1668f4c9e12b
Aadit Kolar <aadit.kolar@gmail.com>
2024-09-08 01:25:05 -0500

    Create read and write functions to/from json for values.py

--- values.py
+++ values.py
@@ -1,4 +1,5 @@
 import RCAIDE
+import json
 
 
 # Geometry Data
@@ -8,10 +9,33 @@ propulsor_names = []
 
 # Aircraft Configs Data
 config_data = []
-aircraft_configs = None
+# aircraft_configs = None
 
 # Analysis Data
 analysis_data = []
 
 # Mission Data
+mission = RCAIDE.Framework.Mission.Sequential_Segments()
 mission_data = []
+
+
+def write_to_json():
+    data = {
+        "geometry_data": geometry_data,
+        "config_data": config_data,
+        "analysis_data": analysis_data,
+        "mission_data": mission_data
+    }
+
+    data_str = json.dumps(data, indent=4)
+    return data_str
+
+
+def read_from_json(data_str):
+    global geometry_data, config_data, analysis_data, mission_data
+
+    data = json.loads(data_str)
+    geometry_data = data["geometry_data"]
+    config_data = data["config_data"]
+    analysis_data = data["analysis_data"]
+    mission_data = data["mission_data"]
