5d3eaab0e2d0139e936396ef28e4e1312475fae7
Aadit Kolar <aadit.kolar@gmail.com>
2026-01-03 03:28:29 +0530

    Added get_data_values functionality to fuel_line_widget.py

--- tabs/geometry/widgets/powertrain/distributors/fuel_line_widget.py
+++ tabs/geometry/widgets/powertrain/distributors/fuel_line_widget.py
@@ -1,10 +1,11 @@
 import RCAIDE
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QTabWidget, QPushButton, QLineEdit
 
+from tabs.geometry.widgets import GeometryDataWidget
 from widgets import DataEntryWidget
 
 
-class FuelLineWidget(QWidget):
+class FuelLineWidget(GeometryDataWidget):
     def __init__(self, index, on_delete, data_values=None):
         super(FuelLineWidget, self).__init__()
 
@@ -21,10 +22,11 @@ class FuelLineWidget(QWidget):
         self.section_name_edit = QLineEdit(self)
         self.name_layout.addWidget(QLabel("Fuel Line Name: "))
         self.name_layout.addWidget(self.section_name_edit)
-        layout.addLayout(self.name_layout) 
+        layout.addLayout(self.name_layout)
 
         delete_button = QPushButton("Delete Fuel Line", self)
-        delete_button.setStyleSheet("color:#dbe7ff; font-weight:500; margin:0; padding:0;")
+        delete_button.setStyleSheet(
+            "color:#dbe7ff; font-weight:500; margin:0; padding:0;")
         delete_button.clicked.connect(self.delete_button_pressed)
 
         layout.addWidget(delete_button)
@@ -32,9 +34,18 @@ class FuelLineWidget(QWidget):
         self.setLayout(layout)
 
         if data_values:
-            self.load_data_values(data_values, index)
+            self.load_data_values(data_values)
 
-    def delete_button_pressed(self): 
+    def get_data_values(self):
+        data = {"name": self.section_name_edit.text()}
+        return data, self.create_rcaide_structure(data)
+
+    def create_rcaide_structure(self, data):
+        line = RCAIDE.Library.Components.Powertrain.Distributors.Fuel_Line()
+        line.tag = data["name"]
+        return line
+
+    def delete_button_pressed(self):
         if self.on_delete is None:
             print("on_delete is None")
             return
@@ -42,7 +53,7 @@ class FuelLineWidget(QWidget):
         self.on_delete(self.index)
 
     def create_scroll_layout(self):
-        # Create a widget to contain the layoutd
+        # Create a widget to contain the layout
         scroll_content = QWidget()
         # Set the main layout inside the scroll content
         layout = QVBoxLayout(scroll_content)
