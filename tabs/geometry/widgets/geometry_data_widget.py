f2cc024cffd3039756ba5f0c66c63d5bc523ea29
Aadit Kolar <aadit.kolar@gmail.com>
2026-01-03 02:51:50 +0530

    Create GeometryDataWidget

---
+++ tabs/geometry/widgets/geometry_data_widget.py
@@ -0,0 +1,18 @@
+from PyQt6.QtWidgets import QWidget
+
+
+class GeometryDataWidget(QWidget):
+    def __init__(self):
+        super(GeometryDataWidget, self).__init__()
+
+    def create_rcaide_structure(self, data):
+        pass
+
+    def get_data_values(self):
+        pass
+
+    def load_data_values(self):
+        pass
+
+    def delete_button_pressed(self):
+        pass
