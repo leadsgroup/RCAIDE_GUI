464bb030dcf43b42283fbfd4eef78042294422dd
Aadit Kolar <aadit.kolar@gmail.com>
2026-01-05 02:14:29 +0530

    Added functionality to turboelectric_generator_widget.py

--- tabs/geometry/widgets/powertrain/converters/turboelectric_generator_widget.py
+++ tabs/geometry/widgets/powertrain/converters/turboelectric_generator_widget.py
@@ -1,10 +1,11 @@
-import RCAIDE
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QTabWidget, QPushButton, QLineEdit
+from RCAIDE.Library.Components.Powertrain.Converters import Turboelectric_Generator
 
+from tabs.geometry.widgets import GeometryDataWidget
 from widgets import DataEntryWidget
 
 
-class TurboelectricGeneratorWidget(QWidget):
+class TurboelectricGeneratorWidget(GeometryDataWidget):
     def __init__(self, index, on_delete, data_values=None):
         super(TurboelectricGeneratorWidget, self).__init__()
 
@@ -21,20 +22,34 @@ class TurboelectricGeneratorWidget(QWidget):
         self.section_name_edit = QLineEdit(self)
         self.name_layout.addWidget(QLabel("Converter Name: "))
         self.name_layout.addWidget(self.section_name_edit)
-        layout.addLayout(self.name_layout) 
+        layout.addLayout(self.name_layout)
 
-        #delete_button = QPushButton("Delete Converter", self)
-        #delete_button.setStyleSheet("color:#dbe7ff; font-weight:500; margin:0; padding:0;")
-        #delete_button.clicked.connect(self.delete_button_pressed)
+        # delete_button = QPushButton("Delete Converter", self)
+        # delete_button.setStyleSheet("color:#dbe7ff; font-weight:500; margin:0; padding:0;")
+        # delete_button.clicked.connect(self.delete_button_pressed)
 
-        #layout.addWidget(delete_button)
+        # layout.addWidget(delete_button)
         layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(layout)
 
         if data_values:
             self.load_data_values(data_values, index)
 
-    def delete_button_pressed(self): 
+    def get_data_values(self):
+        title = self.section_name_edit.text()
+        # data = self.data_entry_widget.get_values()
+        data = {"Converter Name": title}
+        # data_si = self.data_entry_widget.get_values_si()
+        data_si = {}
+        return data, self.create_rcaide_structure(data_si)
+
+
+    def create_rcaide_structure(self, data):
+        te = Turboelectric_Generator()
+        te.tag = self.section_name_edit.text()
+        return te
+
+    def delete_button_pressed(self):
         if self.on_delete is None:
             print("on_delete is None")
             return
