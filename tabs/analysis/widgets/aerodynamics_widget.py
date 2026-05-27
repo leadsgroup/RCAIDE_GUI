70634738e21b9e0ed0691243215b6158e58ef592
Matthew Clarke <mclarke2@stanford.edu>
2026-05-27 14:37:18 -0700

    Update aerodynamics_widget.py

--- tabs/analysis/widgets/aerodynamics_widget.py
+++ tabs/analysis/widgets/aerodynamics_widget.py
@@ -1,4 +1,5 @@
 import json
+import os
 from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout, QScrollArea, \
     QComboBox
 
@@ -24,7 +25,7 @@ class AerodynamicsWidget(AnalysisDataWidget):
         self.data_entry_widget = DataEntryWidget(self.data_units_labels[0], num_cols=1)
 
         # Load defaults
-        with open("app_data/defaults/aerodynamic_analysis.json", "r") as defaults:
+        with open(os.path.join("app_data", "defaults", "aerodynamic_analysis.json"), "r") as defaults:
             self.defaults = json.load(defaults)
 
         self.data_entry_widget.load_data(self.defaults)
